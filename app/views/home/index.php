30f95da2d11f753163a4817bac45359e0f9938fc
IopeRan <chilliope48@gmail.com>
2023-06-03 04:12:19 +0800

    gw ngantuk

---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="jumbotron jumbotron-fluid mt-5">
+        <div class="container bg-dark text-white p-5 rounded">
+            <h1 class="display-4">Selamat Datang!</h1>
+            <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+            <hr>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quae labore veniam fuga eaque, eos facere numquam in necessitatibus voluptatum maxime explicabo, quam odit, neque consectetur cumque. Laboriosam, nihil quos.</p>
+            <button class="btn btn-primary">Learn MVC</button>
+        </div>
+    </div>
+</div>
